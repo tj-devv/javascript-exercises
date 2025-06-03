b411103508153dad2ccb13bfe1e7ace873c390b9
tjdev <jjjaja001@gmail.com>
2025-06-03 12:34:47 +0100

    05_joinStrings: Solve string concatenation exercise
    This commit addresses the "joinStrings" exercise by:
    
    - Implementing the string concatenation logic to create the `fullName` and `greeting` variables.
    - Calculating the `age` variable by subtracting `birthYear` from `thisYear`.
    - Ensuring that the `greeting` variable correctly incorporates the `fullName` and `age` variables.
    - Updating the test file to remove `skip` from tests.
    - Adding example file to show the solution.

--- 05_joinStrings/joinStrings-example.js
+++ 05_joinStrings/joinStrings-example.js
@@ -2,8 +2,10 @@ const birthYear = 1947;
 const thisYear = 1965;
 const firstName = "Carlos";
 const lastName = "Stevenson";
+const fullName = firstName + " " + lastName;
+const age = thisYear - birthYear;
 
-const greeting = "Hello! My name is " + firstName + " " + lastName + " and I am " + (thisYear - birthYear) + " years old.";
+const greeting = "Hello! My name is " + fullName + " and I am " + age + " years old.";
 
 console.log(greeting);
 
@@ -12,5 +14,5 @@ module.exports = {
   birthYear,
   thisYear,
   firstName,
-  lastName
-}
+  lastName,
+};
